9cfabd833860f76fe6f33da9ce593f16c0d88a6b
Kalininaaw16012002 <kalinina_asya_1601@mail.ru>
2025-10-02 19:55:06 +0300

    Дз выполнено

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ import { fetchAndRenderComments } from './fetchAndRenderComments.js'
 import { updateComments } from './comments.js'
 import { renderComments } from './renderComments.js'
 
+document.querySelector('.comments').innerHTML =
+    'Пожалуйста, подождите, комментарии загружаются...'
+
 fetchAndRenderComments().then((data) => {
     updateComments(data)
     renderComments()
