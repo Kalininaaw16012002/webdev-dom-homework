b3aaac7ea2d26678b48d66abd3eb9eae12604670
Kalininaaw16012002 <kalinina_asya_1601@mail.ru>
2025-10-20 13:12:38 +0300

    Ошибка токена

--- fetchAndRenderComments.js
+++ fetchAndRenderComments.js
@@ -64,7 +64,7 @@ export const postComments = (text, name) => {
             return fetchAndRenderComments()
         })
 }
-
+ 
 export const login = (login, password) => {
     return fetch(authHost + '/login', {
         method: 'POST',
